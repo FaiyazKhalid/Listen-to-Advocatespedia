9ea3194a3f3d90f8248f4c8d344cc54f5d8d68dd
FaiyazKhalid <fkhalid42@gmail.com>
2024-07-04 00:39:16 +0530

    Update display.php

--- display.php
+++ display.php
@@ -3,7 +3,7 @@ require_once('dbconfig.php');
 
 //display the counter of the "real" table
 
-$query = mysqli_query($con, 'SELECT SUM(page_counter) AS c FROM mw_hit_counter');
+$query = mysqli_query($con, 'SELECT SUM(page_counter) AS c FROM wiki_hit_counter');
 $array = mysqli_fetch_array($query);
 
 echo $array['c'];
